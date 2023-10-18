6dbd9f584498f7de2d54b555846b2de5156aab0a
Anuradha <anu@Anuradhas-MacBook-Pro.local>
2023-10-19 01:07:59 +0530

    react project with scratch

---
+++ webpack.config.ts
@@ -0,0 +1,42 @@
+import * as webpack from "webpack";
+import * as HtmlWebPackPlugin from "html-webpack-plugin";
+import * as MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const prod = process.env.NODE_ENV === 'production';
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html"
+});
+
+const config: webpack.Configuration = {
+  mode: prod ? 'production' : 'development',
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + '/dist/',
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: [/node_modules/],
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    htmlPlugin,
+    new MiniCssExtractPlugin(),
+  ]
+};
+
+export default config;
